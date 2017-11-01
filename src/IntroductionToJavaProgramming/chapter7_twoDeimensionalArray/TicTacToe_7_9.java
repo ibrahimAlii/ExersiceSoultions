a62c794613e499829f6e5166080b813f532bec9f
Ibrahim <sun_moon545@yahoo.com>
2017-11-01 15:49:27 +0200

    initial commit.

--- src/IntroductionToJavaProgramming/chapter7_twoDeimensionalArray/TicTacToe_7_9.java
+++ src/IntroductionToJavaProgramming/chapter7_twoDeimensionalArray/TicTacToe_7_9.java
@@ -16,7 +16,7 @@ public class TicTacToe_7_9 {
 
     }
 
-    public static void displayGame(Scanner input, String[][] x_O) {
+    private static void displayGame(Scanner input, String[][] x_O) {
         String currentPlayer;
         int row = 0, column = 0;
         for (int i = 0; i < x_O.length; i++) {
@@ -24,7 +24,7 @@ public class TicTacToe_7_9 {
                 currentPlayer = (x_O[row][column].equals("X") ? "O" : "X");
                 System.out.print("Enter a row (0, 1, or 2): " + currentPlayer + " ");
                 row = input.nextInt();
-                System.out.print("\nEnter a column (0, 1, or 2): " + currentPlayer + " ");
+                System.out.print("Enter a column (0, 1, or 2): " + currentPlayer + " ");
                 column = input.nextInt();
                 x_O[row][column] = currentPlayer;
                 printForm(x_O);
@@ -44,26 +44,30 @@ public class TicTacToe_7_9 {
 
     private static boolean thereIsDraw(String[][] x_o, String currentPlayer, int currentRow, int currentColumn) {
 
+        boolean isDraw = false;
         for (int i = 0; i < x_o.length; i++) {
             for (int j = 0; j < x_o[i].length; j++) {
-                if (!x_o[i][j] .equals(" ") && !thereIsWinner(x_o, currentPlayer, currentRow, currentColumn)) {
-                    System.out.println("It's a draw!");
-                    return true;
+                if (!x_o[i][j].equals(" ") && !thereIsWinner(x_o, currentPlayer, currentRow, currentColumn)) {
+                    isDraw = true;
+                }else{
+                    isDraw = false;
+                    break;
                 }
             }
         }
-        return false;
+        System.out.print((isDraw ? "It's a draw!\n" : ""));
+        return isDraw;
     }
 
     private static boolean thereIsWinner(String[][] x_o, String currentPlayer, int currentRow, int currentColumn) {
 
-
+                    //check rows
             if (x_o[currentRow][0].equals(currentPlayer) &&  x_o[currentRow][1].equals(currentPlayer) && x_o[currentRow][2].equals(currentPlayer)
-                    ||
+                    || // check columns
                     x_o[0][currentColumn].equals(currentPlayer) && x_o[1][currentColumn].equals(currentPlayer) && x_o[2][currentColumn].equals(currentPlayer)
-                    ||
+                    ||  // check diagonal
                     currentRow == currentColumn && x_o[0][0].equals(currentPlayer) && x_o[1][1].equals(currentPlayer) && x_o[2][2].equals(currentPlayer)
-                    ||
+                    ||  // check opposite diagonal
                     currentRow + currentColumn == 2 && x_o[1][1].equals(currentPlayer) && x_o[0][2].equals(currentPlayer) && x_o[2][0].equals(currentPlayer)) {
                 System.out.println("We have a winner: " + currentPlayer);
                 return true;
