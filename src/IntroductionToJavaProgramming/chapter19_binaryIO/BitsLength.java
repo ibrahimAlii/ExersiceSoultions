5097798dec9d1fa9a3c5842af3674499b947d02f
Ibrahim <sun_moon545@yahoo.com>
2018-03-11 17:24:24 +0200

    commit

---
+++ src/IntroductionToJavaProgramming/chapter19_binaryIO/BitsLength.java
@@ -0,0 +1,35 @@
+package chapter19;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BitsLength {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a file name: ");
+        String filename = input.nextLine();
+
+        FileInputStream in = new FileInputStream(filename);
+
+        int value;
+        while ((value = in.read()) != -1) {
+            System.out.print(getBits(value));
+        }
+
+        in.close();
+    }
+
+    public static String getBits(int value) {
+        String result = "";
+
+        int mask = 1;
+        for (int i = 7; i >= 0; i--) {
+            int temp = value >> i;
+            int bit = temp & mask;
+            result = result + bit;
+        }
+        return result;
+    }
+
+}
