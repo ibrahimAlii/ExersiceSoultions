f46dd63e04060b930e9b5e1978845d4b17decf30
Ibrahim <sun_moon545@yahoo.com>
2018-03-27 22:21:43 +0200

    commit

---
+++ src/IntroductionToJavaProgramming/chapter23_setAndMaps/CountOccurrencesNumber23_6.java
@@ -0,0 +1,42 @@
+package chapter23;
+
+import java.util.*;
+
+public class CountOccurrencesNumber23_6 {
+
+    public static void main(String[] gg) {
+
+        Scanner input = new Scanner(System.in);
+
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        while (true) {
+
+            int number = input.nextInt();
+
+            if (number == 0) break;
+
+
+            if (map.get(number) != null) {
+                int value = (map.get(number));
+                value++;
+                map.put(number, value);
+            } else {
+                map.put(number, 1);
+            }
+
+        }
+
+        int max = Collections.max(map.values());
+        Set keys = map.keySet();
+
+        for (Object key : keys) {
+            Integer value = map.get(key);
+            if (value.equals(max))
+                System.out.println("Number " + key + " Occured most");
+        }
+    }
+
+
+}
