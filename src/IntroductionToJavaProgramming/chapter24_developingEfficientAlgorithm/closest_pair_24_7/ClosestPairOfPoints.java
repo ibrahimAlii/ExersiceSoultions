2776c1512973c26b85ad183e2e1dd163b4ae03d9
Ibrahim Ali <sun_moon545@yahoo.com>
2018-08-20 13:51:40 +0200

    Exercise 24.8

--- src/IntroductionToJavaProgramming/chapter24_developingEfficientAlgorithm/closest_pair_24_7/ClosestPairOfPoints.java
+++ src/IntroductionToJavaProgramming/chapter24_developingEfficientAlgorithm/closest_pair_24_7/ClosestPairOfPoints.java
@@ -7,24 +7,30 @@ import java.util.List;
 
 public class ClosestPairOfPoints {
 
-    public static final int LIST_SIZE = 100;
+    public static final int LIST_SIZE = 10000000;
 
     private static List<Point> pointsOrderedOnY = new ArrayList<>();
     private static List<Point> S1 = new ArrayList<>();
     private static List<Point> S2 = new ArrayList<>();
     private static Point midPoint;
 
+    private static int d1, d2, d3;
+
     public static void main(String[] args) {
 
 
         // Fill the data
+        long start = System.currentTimeMillis();
         for (int i = 0; i < LIST_SIZE; i++) {
             pointsOrderedOnY.add(new Point(Math.random() * 100, Math.random() * 100));
         }
+        System.out.println(System.currentTimeMillis() - start);
 
+        long startTime = System.currentTimeMillis();
         // Sort the list
         Collections.sort(pointsOrderedOnY, new CompareY());
 
+        System.out.println(System.currentTimeMillis() - startTime);
         // Obtain mid point
         midPoint = pointsOrderedOnY.get(pointsOrderedOnY.size() / 2);
 
@@ -32,9 +38,9 @@ public class ClosestPairOfPoints {
         S1 = pointsOrderedOnY.subList(0, pointsOrderedOnY.size() / 2);
         S2 = pointsOrderedOnY.subList((pointsOrderedOnY.size() / 2) + 1, pointsOrderedOnY.size());
 
-        for (Point point :
-                pointsOrderedOnY) {
-            if (S1.contains(point) && (midPoint.getX() - point.getX()) <=)
+        // TODO: 19/08/2018 complete this
+        for (Point point : pointsOrderedOnY) {
+//            if (S1.contains(point) && (midPoint.getX() - point.getX()) <= )
 
         }
 
