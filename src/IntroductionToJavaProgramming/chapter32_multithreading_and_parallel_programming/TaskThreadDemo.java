5cb87f5663873bce4629f67e2fb40161a75a3521
Ibrahim Ali <sun_moon545@yahoo.com>
2018-10-27 20:29:58 +0200

    Exercise 32.1 [Threads]

---
+++ src/IntroductionToJavaProgramming/chapter32_multithreading_and_parallel_programming/TaskThreadDemo.java
@@ -0,0 +1,52 @@
+package IntroductionToJavaProgramming.chapter32_multithreading_and_parallel_programming;
+
+public class TaskThreadDemo {
+
+    public static void main(String[] args) {
+        PrintChar printA = new PrintChar('a', 100);
+        PrintChar printB = new PrintChar('b', 100);
+        PrintNum printNum = new PrintNum(100);
+
+        Thread thread1 = new Thread(printA);
+        Thread thread2 = new Thread(printB);
+        Thread thread3 = new Thread(printNum);
+
+        thread3.start();
+        thread1.start();
+        thread2.start();
+    }
+
+
+}
+
+ class PrintChar implements Runnable {
+    public char aChar;
+    public int times;
+
+    public PrintChar(char c, int times) {
+        aChar = c;
+        this.times = times;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < times; i++) {
+            System.out.print(aChar);
+        }
+    }
+}
+
+ class PrintNum implements Runnable {
+    public int lastNum;
+
+    public PrintNum(int n) {
+        lastNum = n;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= lastNum; i++) {
+            System.out.print(" " + i);
+        }
+    }
+}
